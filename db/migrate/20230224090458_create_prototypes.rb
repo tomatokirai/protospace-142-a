c4e0bcfd01c6f8ba344a2eda05647e28c7febc4f
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-02-24 18:34:07 +0900

    prototypeのテーブルの作成

--- db/migrate/20230224090458_create_prototypes.rb
+++ db/migrate/20230224090458_create_prototypes.rb
@@ -1,7 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration[6.0]
   def change
     create_table :prototypes do |t|
-
+      t.string      :title
+      t.text        :catch_copy
+      t.text        :concept
+      t.references  :user_id
       t.timestamps
     end
   end
