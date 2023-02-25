53f730e4bb379bbaf9bb847997f3bb79d3abd532
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-02-26 03:18:33 +0900

    usersテーブルのカラムにプロフィール、所属、役職を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_25_141553) do
+ActiveRecord::Schema.define(version: 2023_02_25_181312) do
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2023_02_25_141553) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "profile"
+    t.text "occupation"
+    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
