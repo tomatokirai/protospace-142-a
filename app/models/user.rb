f484983f262a55756b4a4bee888af7c6aed173ca
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-02-27 00:35:38 +0900

    ApplicationControllerに追加カラムのストロングパラメーターの定義
    エラーは解消　sequel proで保存の確認OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
   validates :profile, presence: true
-  validates :occupation, :presence: true
+  validates :occupation, presence: true
   validates :position, presence: true
 end
