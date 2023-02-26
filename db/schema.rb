4464a623fa58418032b1d91ab9762f2b4103ae9c
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-02-26 23:43:42 +0900

    新規登録画面の編集とvalidatesの設定
    ログイン時にエラーが発生している状態

--- db/schema.rb
+++ db/schema.rb
@@ -26,14 +26,14 @@ ActiveRecord::Schema.define(version: 2023_02_25_181312) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "profile"
-    t.text "occupation"
-    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
