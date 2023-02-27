e11da44d76a81724c404128ca8d3773da483f9ee
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-02-27 20:50:01 +0900

    updateのルーティング、コンロローラーの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,6 +24,12 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    redirect_to action: :index
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
