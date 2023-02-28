aa50f37fa5a9a2e8be7e48f299058b53fae3084d
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-02-28 12:34:47 +0900

    commentのバリデーションとルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
 
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show] do
+    resources :comments, only: [:index, :create]
+  end
 
 end
